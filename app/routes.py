01f3468d72b92e6457891779d1938dbc30ac2f5e
Nicole Mejia <snicolemejia@gmail.com>
2022-12-24 13:02:08 -0500

    added five day forecast endpoint

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,7 @@ weather_key = os.environ.get("WEATHER_KEY")
 @proxy_bp.route("/location", methods=["GET"])
 def get_lat_lon():
     loc_query = request.args.get("q")
+    print("LOC QUERY", loc_query)
     if not loc_query:
         return {"message": "must provide q parameter (location)"}
 
@@ -28,13 +29,38 @@ def get_lat_lon():
 def get_weather():
     lat_query = request.args.get("lat")
     lon_query = request.args.get("lon")
-    print(lat_query, lon_query)
+    # print(request.args)
+    print("lat_query: ", lat_query, "lon_query: ", lon_query)
     if not lat_query or not lon_query:
         return {"message": "must provide lat and lon parameters"}
 
     response = requests.get(
         "https://api.openweathermap.org/data/2.5/weather",
-        params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+        params={
+            "lat": lat_query,
+            "lon": lon_query,
+            "appid": weather_key,
+            "units": "imperial"
+            }
     )
     return response.json()
 
+@proxy_bp.route("/forecast", methods=["GET"])
+def get_forecast():
+    lat_query = request.args.get("lat")
+    lon_query = request.args.get("lon")
+    # print(request.args)
+    print("lat_query: ", lat_query, "lon_query: ", lon_query)
+    if not lat_query or not lon_query:
+        return {"message": "must provide lat and lon parameters"}
+
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/forecast",
+        params={
+            "lat": lat_query, 
+            "lon": lon_query, 
+            "appid": weather_key,
+            "units": "imperial"
+            }
+    )
+    return response.json()
